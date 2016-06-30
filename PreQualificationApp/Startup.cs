3e3496ac7a687e92e0fb18de5b478bb3604730e4
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 10:29:45 -0400

    add project ( default template )

---
+++ PreQualificationApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreQualificationApp.Startup))]
+namespace PreQualificationApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
