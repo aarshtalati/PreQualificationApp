98327c963d9ba135c6fe8c879e68b881687cb0b5
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 10:48:03 -0400

    add BaseUrlHelper class

---
+++ PreQualificationApp/Helpers/BaseUrlHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+
+namespace PreQualificationApp.Helpers
+{
+	public class BaseUrlHelper
+	{
+		public static string getBaseUrl(bool appendHash = true)
+		{
+			var request = HttpContext.Current.Request;
+			var appUrl = HttpRuntime.AppDomainAppVirtualPath;
+			var baseUrl = string.Format("{0}://{1}{2}", request.Url.Scheme, request.Url.Authority, appUrl);
+			if (!appendHash && !baseUrl.EndsWith("/"))
+				baseUrl += "/";
+			else if (appendHash && !baseUrl.EndsWith("/#"))
+				baseUrl += "/#";
+			var response = HttpContext.Current.Response;
+			return baseUrl;
+		}
+	}
+}
