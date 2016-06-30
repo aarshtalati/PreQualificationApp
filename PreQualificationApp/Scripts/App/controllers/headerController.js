05abe81191360fccbf81fe7df9b4c4956448d699
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 11:14:12 -0400

    add headerController and spaController

---
+++ PreQualificationApp/Scripts/App/controllers/headerController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('headerController', headerController);
+
+	headerController.$inject = ['$scope', '$location', '$route'];
+
+	function headerController($scope, $location, $route) {
+		$scope.title = 'HeaderController';
+
+		$scope.isActive = function (viewLocation) {
+			return viewLocation === $location.path();
+		};
+
+		$scope.getHref = function (viewLocation) {
+			//var test1 = $route.routes;
+			viewLocation = (rootUrl + viewLocation);
+			return viewLocation;
+		}
+	}
+})();
---
+++ PreQualificationApp/Scripts/App/controllers/spaController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('spaController', ['$scope', '$location', spaController]);
+
+	function spaController($scope, $location) {
+		$scope.title = 'spaController';
+
+		$scope.changeView = function (view) {
+			$location.path(view);
+		};
+		activate();
+
+		function activate() { }
+	}
+})();
