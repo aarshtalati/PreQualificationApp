1b21810365b13128e68337696d07355500f8240b
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 10:53:47 -0400

    add Ng dir structure

---
+++ PreQualificationApp/Scripts/App/app.js
@@ -0,0 +1,20 @@
+﻿var spaBaseUrl = '/';
+
+var app = angular.module('app', [
+	// Angular modules 
+
+	'ng', 'ngRoute', 'ngSanitize', 'ngAnimate',
+
+	// Custom modules 
+
+	// 3rd Party Modules
+	'toastr'
+
+	//, 'ui.bootstrap', 'ui'
+	//, 'kendo.directives'
+
+]);
+
+
+
+app.value('$', $);
