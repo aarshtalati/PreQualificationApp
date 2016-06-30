98008cc016f82fc94b48ac7370bd053292ca0e9c
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 13:23:26 -0400

    add unit test  - but Jasmine not installed on my station - skipping

---
+++ PreQualificationApp/Scripts/App/test/controllers/customerRepControllerTest.js
@@ -0,0 +1,30 @@
+﻿//describe('customerRepController', function () {
+//	var $controller,
+//		$httpBackend,
+//		$rootScope;
+
+//	beforeEach(function () {
+//		module('app');
+//		inject(function (_$controller_, _$httpBackend_, _$rootScope_) {
+//			$controller = _$controller_;
+//			$httpBackend = _$httpBackend_;
+//			$rootScope = _$rootScope_;
+//		});
+//	});
+
+//	it('should exist', function () {
+//		//TODO: expectation goes here
+//	});
+
+//	it('should return >1 customer representatives', function () {
+//		var $scope = {};
+//		var cntroller = $controller('customerRepController', { $scope: $scope });
+//		$scope.LoadCustReps();
+//		expect($scope.custReps.length > 0).toBeTruthy();
+//	});
+//});
+
+
+
+
+// just found out that I do not have Jasmine installed :-/
