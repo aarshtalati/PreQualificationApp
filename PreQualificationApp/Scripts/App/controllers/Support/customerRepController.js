385644a24a6861616da12806d4463a0092f2dc9f
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 11:55:55 -0400

    backup plan : support page

---
+++ PreQualificationApp/Scripts/App/controllers/Support/customerRepController.js
@@ -0,0 +1,79 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('customerRepController', customerRepController);
+
+	customerRepController.$inject = ['$scope', 'customerRepService'];
+
+	function customerRepController($scope, service) {
+
+		$scope.title = 'customerRepController';
+
+		$scope.custReps = [];
+		$scope.predicate = '';
+		$scope.reverse = false;
+		$scope.showFilterRow = false;
+		$scope.loadingCustReps = false;
+		$scope.loadingConsumers = true;
+		$scope.emptyTableMessage = false;
+		$scope.selectedCusRepId = undefined;
+
+
+		$scope.search = {};
+		$scope.search.Id = undefined;
+		$scope.search.Name = undefined;
+		$scope.search.State = undefined;
+		$scope.search.Phone = undefined;
+		$scope.search.Email = undefined;
+
+
+		function activate() {
+			$scope.LoadCustReps();
+
+		}
+
+		$scope.LoadCustReps = function () {
+			$scope.custReps = [];
+			$scope.loadingCustReps = true;
+			service.GetAll().success(function (records) {
+				$scope.custReps = records;
+				angular.forEach($scope.custReps, function (value, key) {
+					var fullName = value.fname + ' ' + value.lname;
+					$scope.custReps[key].Name = fullName;
+				});
+				$scope.emptyTableMessage = ($scope.custReps.length === 0);
+				$scope.loadingCustReps = false;
+			}).error(function (data, status) {
+				console.log('Error getting customer representatives', status);
+				console.log(data);
+				$scope.custReps = [];
+				$scope.loadingCustReps = false;
+			});
+		};
+
+
+		$scope.custRepTableSort = function (p) {
+			$scope.predicate = p;
+			$scope.reverse = !$scope.reverse;
+		};
+
+		$scope.toggleFilterVisibility = function () {
+			$scope.showFilterRow = !($scope.showFilterRow);
+			$scope.clearFilter();
+		};
+
+		$scope.clearFilter = function () {
+			$scope.search = {};
+		};
+
+		$scope.$watch('search.Id', function () {
+			if ($scope.search.Id === undefined || $scope.search.Id === null) {
+				$scope.clearFilter();
+			}
+		});
+
+		activate();
+	}
+})();
--- PreQualificationApp/Scripts/App/services/Support/customerRepService.js
+++ PreQualificationApp/Scripts/App/services/Support/customerRepService.js
@@ -1,1 +1,32 @@
-﻿//TODO : comeback here later
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.service('customerRepService', customerRepService);
+
+	customerRepService.$inject = ['$http'];
+
+	function customerRepService($http) {
+
+		this.GetAll = GetAll;
+
+		function GetAll() {
+			var query = 'http://www.filltext.com/?pretty=true&rows=';
+			query += getRandomInt(0, 1000).toString()
+			query += '&Id={index}&fname={firstName}&lname={lastName}&city={city}&state={usState|abbr}&phone={phone|format}&email={email}';
+
+			return $http({
+				method: 'GET',
+				url: query
+			});
+
+
+			// Returns a random integer between min (included) and max (included)
+			function getRandomInt(min, max) {
+				return Math.floor(Math.random() * (max - min + 1)) + min;
+			}
+
+		}
+	}
+})();
