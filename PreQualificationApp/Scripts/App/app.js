f2d6ca712bc839ca358ef274ff0d49415afe77c2
aarshtalati <aarshstalati1989@gmail.com>
2016-06-30 11:18:33 -0400

    add services

--- PreQualificationApp/Scripts/App/app.js
+++ PreQualificationApp/Scripts/App/app.js
@@ -15,6 +15,44 @@ var app = angular.module('app', [
 
 ]);
 
+app.config(['$routeProvider',
+	function ($routeProvider) {
 
+		//$locationProvider.html5Mode(true);
+
+
+		$routeProvider
+
+			// default url
+			.when(spaBaseUrl, {
+				templateUrl: rootUrl + '/Scripts/App/Views/Registration/Form.html',
+				controller: 'registrarController'
+			})
+
+			.when(spaBaseUrl + 'Register', {
+				templateUrl: rootUrl + '/Scripts/App/Views/Registration/Form.html',
+				controller: 'registrarController'
+			})
+
+			.when(spaBaseUrl + 'Support', {
+				templateUrl: rootUrl + '/Scripts/App/Views/Support/Support.html',
+				controller: 'customerRepController'
+			})
+
+			.when(spaBaseUrl + 'About', {
+				templateUrl: rootUrl + '/Scripts/App/Views/Support/About.html'
+			})
+
+			.when(spaBaseUrl + 'notFound', {
+				templateUrl: rootUrl + '/Scripts/App/Views/Error/NotFound.html'
+			})
+
+			// Invalid route
+			.otherwise({
+				redirectTo: spaBaseUrl + 'notFound'
+			});
+
+	}
+]);
 
 app.value('$', $);
---
+++ PreQualificationApp/Scripts/App/services/Registration/preQualifyService.js
@@ -0,0 +1,20 @@
+﻿(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.service('preQualifyService', ['$http', preQualifyService]);
+
+	function preQualifyService($http) {
+
+		var baseUrl = '';
+
+		this.CustomerPreQualificaionCheck = CustomerPreQualificaionCheck;
+
+		function CustomerPreQualificaionCheck(customer) {
+			var url = "https://api.kabbage.com/"
+			var payload = customer;
+			return $http.post(url, payload)
+		};
+	}
+})();
